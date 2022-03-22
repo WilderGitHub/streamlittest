405b8124100c12481ad09c923d9677878aa0102a
WilderGitHub <wilder_serdan@hotmail.com>
2022-03-22 14:38:10 -0400

    camdfbdfg

--- app1.py
+++ app1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt 
 #import seaborn as sns
 from collections import Counter
 import warnings
@@ -19,3 +19,24 @@ vuelosTrain = pd.read_csv("vuelosTrain.csv")#, header=None)
 vuelosTrain.drop(labels=["Unnamed: 0"],axis=1,inplace=True)
 #vuelosTrain.head()
 st.dataframe(vuelosTrain.head(x))
+
+with st.sidebar:
+    option = st.selectbox(
+     'Calificaciones de acuerdo a servicios ofrecidosHistogramas de:',
+     ("Inflight wifi service", "Departure/Arrival time convenient", "Ease of Online booking", "Gate location", "Food and drink", "Online boarding", "Seat comfort", "Inflight entertainment", "On-board service", "Leg room service", "Baggage handling", "Checkin service", "Inflight service", "Cleanliness"))
+
+#st.write('You selected:', option)
+arr = vuelosTrain[option]
+fig, ax = plt.subplots()
+ax.hist(arr, bins=10)
+st.pyplot(fig)
+
+y = st.selectbox(
+     'Histogramas de variables cuantitativas:',
+     ("Age", "Flight Distance", "Departure Delay in Minutes", "Arrival Delay in Minutes"))
+
+#st.write('You selected:', option)
+arr = vuelosTrain[y]
+fig, ax = plt.subplots()
+ax.hist(arr, bins=50)
+st.pyplot(fig)

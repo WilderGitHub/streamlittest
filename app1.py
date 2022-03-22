af37466ede81073e0d0f064b827e2841c668a043
WilderGitHub <wilder_serdan@hotmail.com>
2022-03-22 11:01:18 -0400

    camb

--- app1.py
+++ app1.py
@@ -2,18 +2,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt 
-import seaborn as sns
+#import seaborn as sns
 from collections import Counter
 import warnings
 warnings.filterwarnings("ignore")
 x = st.slider('head',3,10)
-st.write(x, 'cuadradito', x * x)
+#st.write(x, 'cuadradito', x * x)
 
 #@st.cache
 #def cargar():
 #    tienda = pd.read_csv("store_data.csv")
 #    return tienda
-vuelosTrain = st.cache(pd.read_csv)("vuelosTrain.csv", header=None)
+vuelosTrain = st.cache(pd.read_csv)("vuelosTrain.csv")#, header=None)
 vuelosTrain.drop(labels=["Unnamed: 0"],axis=1,inplace=True)
 #vuelosTest.drop(labels=["Unnamed: 0"],axis=1,inplace=True)
 vuelosTrain.head()

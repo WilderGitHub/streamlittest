38242a1fdf51fbd0626ba2a8dc37dde84c949d17
WilderGitHub <wilder_serdan@hotmail.com>
2022-03-22 10:38:57 -0400

    a ver

--- app1.py
+++ app1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 x = st.slider('Nivel de confianza',3,10)
-st.write(x, 'cuadradin', x * x)
+st.write(x, 'cuadradito', x * x)
 
 #@st.cache
 #def cargar():
